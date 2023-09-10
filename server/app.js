efc1a927cc59f2c34eeed30e0138e47d0869b97f
jagdeesh kushwaha <pwdacait7@gmail.com>
2023-09-10 13:45:28 +0530

    working om=n controller

--- server/app.js
+++ server/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const authRouter = require('./routes/authRoute')
 
+// to parse the json data from every request ( batayega ki ye req data json type hai)
+app.use(express.json())
+
 // by using /api/auth/ we can redirect all routes after this 
 app.use('/api/auth/',authRouter)
 
--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,6 +1,11 @@
 //  Signup Function where we will write logic of signup
 const signup = (req,resp,next) => {
-
+  const {name,email,password,confirmPassword} = req.body;
+  console.log(name,email,password,confirmPassword)
+  return resp.status(200).json({
+    success:true,
+    data:{Hello:"hello"}
+  })
 };
 
 module.exports = {
