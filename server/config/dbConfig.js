3383535b8f94dca207a8b03a7ac608c605c0021a
jagdeesh kushwaha <pwdacait7@gmail.com>
2023-10-01 12:57:13 +0530

    some changes

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,9 +1,9 @@
 const  mongoose = require("mongoose");
-
+const dotenv = require("dotenv")
+dotenv.config()
 const MONGODB_URL = process.env.MONGODB_URL
 
 const dbconnect = ()=>{
-    console.log(MONGODB_URL)
     mongoose.connect(MONGODB_URL)
     .then((conn)=> console.log(conn.connection.host))
     .catch((err)=> console.log(err.message));
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const app = require('./app.js')
-const PORT = 5000;
-
+const dotenv = require("dotenv")
+dotenv.config()
+const PORT = process.env.PORT||5000;
 app.listen(PORT,()=>{
     console.log(`Server is listening at http://localhost:${PORT}`)
 });
