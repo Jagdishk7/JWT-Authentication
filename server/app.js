2f6d108203c02d27148db0d84a21e96590d14df8
jagdeesh kushwaha <pwdacait7@gmail.com>
2023-11-17 13:40:43 +0530

    cookie parser added

--- server/app.js
+++ server/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const authRouter = require('./routes/authRoute');
 const dbconnect = require('./config/dbConfig');
+const cookieParser = require('cookie-parser');
 
 dbconnect();
 
 // to parse the json data from every request ( batayega ki ye req data json type hai)
 app.use(express.json())
+app.use(cookieParser()); // Third-party middleware
 
 // by using /api/auth/ we can redirect all routes after this 
 app.use('/api/auth/',authRouter)
